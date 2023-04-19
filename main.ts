9b06d53c57f90b97481e3599e2aa732bbd1d9a20
PastelMagic <kuwano@pastelmagic.com>
2023-04-19 14:31:29 +0900

    スイッチの二度押しに反応しないようにした

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-let A = 0
 let B = 0
+let A = 0
 let X = 0
+let Y = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        A = 1
-        B = 1
+    if (A == 0 && B == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            A = 1
+            B = 1
+        }
     }
 })
 basic.forever(function () {
@@ -43,12 +46,11 @@ basic.forever(function () {
 basic.forever(function () {
     if (B == 1) {
         music.playMelody("C5 B A G F E D C ", 1000)
-        music.playMelody("C5 B A G F E D C ", 1000)
         B = 0
     }
 })
 basic.forever(function () {
-    led.unplot(X, 2)
+    led.unplot(X, Y)
     if (input.acceleration(Dimension.X) < 0) {
         if (0 < X) {
             X += -1
@@ -58,6 +60,16 @@ basic.forever(function () {
             X += 1
         }
     }
-    led.plot(X, 2)
+    if (input.acceleration(Dimension.Y) < 0) {
+        if (0 < Y) {
+            Y += -1
+        }
+    } else {
+        if (Y < 4) {
+            Y += 1
+        }
+    }
+    basic.pause(100)
+    led.plot(X, Y)
     basic.pause(100)
 })
